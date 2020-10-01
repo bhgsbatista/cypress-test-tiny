3773b1df57adbc53ab9bc4958567e101d38a8bff
Samuel Batista <sbatista@bhg.internal>
2020-10-01 19:03:14 -0400

    Demonstrate the issue with route2 method param

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,30 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.route2('http://dummy.restapiexample.com/api/v1/create').as('create')
+
+    cy.window().then((win) => {
+      win.eval(
+        `fetch("http://dummy.restapiexample.com/api/v1/create", {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        });`
+      )
+    })
+
+    cy.wait('@create').should(({ response }) => expect(response.statusCode).be.equal(200))
+  })
+
+  it('should work', () => {
+    cy.route2('POST', 'http://dummy.restapiexample.com/api/v1/create').as('create')
+
+    cy.window().then((win) => {
+      win.eval(
+        `fetch("http://dummy.restapiexample.com/api/v1/create", {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        });`
+      )
+    })
+
+    cy.wait('@create').should(({ response }) => expect(response.statusCode).be.equal(200))
   })
 })
